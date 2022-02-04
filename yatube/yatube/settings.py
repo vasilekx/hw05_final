8e086ebfe6da855f7ae43a400f898b201cde309e
Владислав Василенко <vasilekx8@yandex.ru>
2022-02-04 09:59:53 +0300

    Update files

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'django_extensions',
     'sorl.thumbnail',
     'posts.apps.PostsConfig',
     'users.apps.UsersConfig',
@@ -54,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+# For Django Debug Toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
